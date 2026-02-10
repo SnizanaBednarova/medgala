330dbc55810232a2cff44ee42ef2eabc1e854107
SnizanaBednarova <snizana.bednarova@ipsos.com>
2026-02-10 21:32:29 +0100

    fixes

--- phpapi/src/Config.php
+++ phpapi/src/Config.php
@@ -19,10 +19,10 @@ class Config
     {
         $this->appUrl = getenv('APP_URL') ?: 'http://localhost:8011';
         $this->gpMerchantNumber = '20160190';
-				$this->gpPublicKey  = __DIR__ . '/../cert/gpe.signing_test.pem';       // GP TEST public cert (na ověřování odpovědí)
-			  $this->gpPrivateKey = __DIR__ . '/../cert/gpwebpay-pvk-test.key';
-				$this->gpPrivateKeyPassword = 'edvk48T5WFEgghr';
-        $this->gpUrl = 'https://test.3dsecure.gpwebpay.com/pgw/order.do';
+				$this->gpPublicKey  = __DIR__ . '/../cert/gpe.signing_prod.pem';       // GP TEST public cert (na ověřování odpovědí)
+			  $this->gpPrivateKey = __DIR__ . '/../cert/gpwebpay-pvk.key';
+				$this->gpPrivateKeyPassword = 'L0DSqTSFQG:HFX';
+        $this->gpUrl = 'https://3dsecure.gpwebpay.com/pgw/order.do';
         $this->smtpHost = getenv('SMTP_HOST') ?: 'smtp.gmail.com';
         $this->smtpPort = (int)(getenv('SMTP_PORT') ?: 587);
         $this->smtpUser = getenv('SMTP_USER') ?: 'ples.ostrava@ifmsa.cz';
