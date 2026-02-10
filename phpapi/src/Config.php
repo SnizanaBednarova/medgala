e72d0454a47a413a7a0e0aa9e11c51b7c4b4e44a
SnizanaBednarova <snizana.bednarova@ipsos.com>
2026-02-10 19:47:58 +0100

    fixes

--- phpapi/src/Config.php
+++ phpapi/src/Config.php
@@ -19,9 +19,9 @@ class Config
     {
         $this->appUrl = getenv('APP_URL') ?: 'http://localhost:8011';
         $this->gpMerchantNumber = '20160190';
-        $this->gpPrivateKey =  __DIR__ . '/../cert/gpwebpay-pvk-test.key';
+        $this->gpPublicKey =  __DIR__ . '/../cert/gpwebpay-pvk-test.key';
         $this->gpPrivateKeyPassword = 'edvk48T5WFEgghr';
-        $this->gpPublicKey =  __DIR__ . '/../cert/gpe.signing_test.pem';
+        $this->gpPrivateKey =  __DIR__ . '/../cert/gpe.signing_test.pem';
         $this->gpUrl = 'https://test.3dsecure.gpwebpay.com/pgw/order.do';
         $this->smtpHost = getenv('SMTP_HOST') ?: 'smtp.gmail.com';
         $this->smtpPort = (int)(getenv('SMTP_PORT') ?: 587);
